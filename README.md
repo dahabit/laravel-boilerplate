c4afad9198ebfbbd7d6b47a44ce57616068a3ad3
Vilhjalmur Magnusson <villimagg@gmail.com>
2012-08-26 15:57:46 +0000

    Added Laravel Framework, Twitter Bootstrap, Composer

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-laravel-bootstrap-composer
-==========================
+Laravel - Bootstrap - Composer
+==============================
 
 Laravel PHP Framework with Twitter Bootstrap and Composer to start developing quickly.
+
+### Laravel framework with Composer and Twitter Bootstrap as bundles to give a head start when starting a new project.
+See [Laravel](http://laravel.com/ "Laravel Website") website for documentation.
---
+++ application/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Laravel: A Framework For Web Artisans</title>
+    <meta name="viewport" content="width=device-width">
+    {{ Asset::container('bootstrapper')->styles() }}
+</head>
+<body>
+    
+    <!-- begin top navbar -->
+    
+    <!-- end top navbar -->
+    
+    <div class="wrapper">
+        
+    </div>
+    
+    <footer>
+        
+    </footer>
+    
+    <!-- begin javascript -->
+    {{ Asset::container('bootstrapper')->scripts() }}
+    <!-- end javascript -->
+</body>
+</html>
