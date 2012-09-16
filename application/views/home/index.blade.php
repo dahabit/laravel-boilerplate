a082d381dbaae725a701546efa69ed3903fa04a7
Vilhjalmur Magnusson <villimagg@gmail.com>
2012-09-16 03:39:32 +0000

    Updates

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -4,9 +4,16 @@
 
     <!-- Main hero unit for a primary marketing message or call to action -->
     <div class="hero-unit">
-        <h1>Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+        <h1><a href="http://laravel.com/">Laravel</a> <i style="text-shadow: 0px -1px 4px white, 0px -2px 10px yellow, 0px -10px 20px #ff8000, 0px -18px 40px red">Boilerplate!</i></h1><br />
+        <p>
+            This is a <strong>Boilerplate</strong> application for Laravel PHP Framework. It includes <a href="http://laravel.com/">Laravel PHP Framework</a>, <a href="http://www.initializr.com/">HTML5 Boilerplate</a>, <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a> bundle for Laravel, <a href="http://getcomposer.org/">Composer</a> bundle for Laravel and a <strong>Sticky Footer</strong> at the bottom. Use it as a starting point to create something more unique.</p><br />
+        <span>
+            
+            <a href="https://github.com/villimagg/laravel-boilerplate/zipball/master" class="btn btn-success btn-large">Download from Github <i class="icon-arrow-down icon-white"></i></a>
+            
+            <a href="https://github.com/villimagg/laravel-boilerplate" class="btn btn-info btn-large">View on Github <i class="icon-eye-open     icon-white"></i></a>
+        
+        </span>
     </div>
 
     <!-- Example row of columns -->
