b00fd1d052a0b46ba2e9219aaec3f1f37811778c
Vilhjalmur Magnusson <villimagg@gmail.com>
2012-09-26 10:06:36 +0000

    Update message

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -7,6 +7,8 @@
         <h1><a href="http://laravel.com/">Laravel</a> <i style="text-shadow: 0px -1px 4px white, 0px -2px 10px yellow, 0px -10px 20px #ff8000, 0px -18px 40px red">Boilerplate!</i></h1><br />
         <p>
             This is a <strong>Boilerplate</strong> application for Laravel PHP Framework. It includes <a href="http://laravel.com/">Laravel PHP Framework</a>, <a href="http://www.initializr.com/">HTML5 Boilerplate</a>, <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a> bundle for Laravel, <a href="http://getcomposer.org/">Composer</a> bundle for Laravel and a <strong>Sticky Footer</strong> at the bottom. Use it as a starting point to create something more unique.</p><br />
+        <h3>Update: 09-26-2012</h3>
+        <p>Updated Laravel to v3.2.8 and Twitter Bootstrap to the latest (with jquery v.1.8.1)</p>
         <span>
             
             <a href="https://github.com/villimagg/laravel-boilerplate/zipball/master" class="btn btn-success btn-large">Download from Github <i class="icon-arrow-down icon-white"></i></a>
